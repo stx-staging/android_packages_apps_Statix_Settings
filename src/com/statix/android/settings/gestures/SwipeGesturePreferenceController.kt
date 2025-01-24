fa08c8acb2d1f591933ed3257b2ee66ddbb09c3f
ralph950412 <ralph950412@gmail.com>
2025-01-25 02:24:39 +0800

    StatixSettings: Add Circle to Search invocation to corner swipe gesture
    Change-Id: I3c21952bc8b859d1961ea85f50c71e5820a0595b

--- src/com/statix/android/settings/gestures/SwipeGesturePreferenceController.kt
+++ src/com/statix/android/settings/gestures/SwipeGesturePreferenceController.kt
@@ -62,6 +62,7 @@ class SwipeGesturePreferenceController(private val context: Context) :
         when (assistAction) {
           0 -> R.string.screenshot_corner_gesture_summary
           1 -> R.string.camera_corner_gesture_summary
+          2 -> R.string.search_corner_gesture_summary
           else -> com.android.settings.R.string.assistant_corner_gesture_summary
         }
       )
--- src/com/statix/android/settings/gestures/SwipeGesturePreferenceFragment.kt
+++ src/com/statix/android/settings/gestures/SwipeGesturePreferenceFragment.kt
@@ -34,6 +34,7 @@ class SwipeGesturePreferenceFragment : RadioButtonPickerFragment() {
       KEY_ASSIST_ACTION -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, -1)
       KEY_SCREENSHOT_ACTION -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, 0)
       KEY_CAMERA_ACTION -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, 1)
+      KEY_SEARCH_ACTION -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, 2)
       else -> {}
     }
   }
@@ -59,6 +60,12 @@ class SwipeGesturePreferenceFragment : RadioButtonPickerFragment() {
         KEY_CAMERA_ACTION,
         /* enabled= */ true,
       ),
+      CandidateInfoExtra(
+        resources.getText(R.string.search_action_title),
+        resources.getText(R.string.search_action_summary),
+        KEY_SEARCH_ACTION,
+        /* enabled= */ true,
+      ),
     )
   }
 
@@ -67,6 +74,7 @@ class SwipeGesturePreferenceFragment : RadioButtonPickerFragment() {
     return when (Settings.Secure.getInt(contentResolver, SETTING_ASSIST_ACTION, -1)) {
       0 -> KEY_SCREENSHOT_ACTION
       1 -> KEY_CAMERA_ACTION
+      2 -> KEY_SEARCH_ACTION
       else -> KEY_ASSIST_ACTION
     }
   }
@@ -76,6 +84,7 @@ class SwipeGesturePreferenceFragment : RadioButtonPickerFragment() {
     when (key) {
       KEY_SCREENSHOT_ACTION -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, 0)
       KEY_CAMERA_ACTION -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, 1)
+      KEY_SEARCH_ACTION -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, 2)
       else -> Settings.Secure.putInt(contentResolver, SETTING_ASSIST_ACTION, -1)
     }
     return true
@@ -135,5 +144,6 @@ class SwipeGesturePreferenceFragment : RadioButtonPickerFragment() {
     private const val KEY_ASSIST_ACTION = "assistant"
     private const val KEY_CAMERA_ACTION = "camera"
     private const val KEY_SCREENSHOT_ACTION = "screenshot"
+    private const val KEY_SEARCH_ACTION = "search"
   }
 }
