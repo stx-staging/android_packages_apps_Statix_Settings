170f425b7a32ee592a819d4e84838db34eb02481
ralph950412 <ralph950412@gmail.com>
2026-06-17 02:37:31 +0800

    Revert "[WIP] Import localepicker stuff"
    Until we update this for cp2a.
    
    This reverts commit 2ecc75eee40c93823fc5529091f074651f20cf79.
    
    Change-Id: Ie20e74ac10d8e686724e2800f0612093e94993d1

--- src/com/statix/android/settings/localepicker/LocaleSelectedListener.java
+++
@@ -1,7 +0,0 @@
-package com.google.android.settings.localepicker;
-
-import com.android.internal.app.LocaleStore;
-
-public interface LocaleSelectedListener {
-    void onLocaleSelected(LocaleStore.LocaleInfo localeInfo);
-}
